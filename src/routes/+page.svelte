c4a97d4a1be2d1c750c3290192fa0a9a08bcb9fc
Gavin Raine Dizon <gavinrainedizon@gmail.com>
2024-04-27 12:24:20 +0800

    feature: landing page UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,27 @@
 </script>
 
 <svelte:head>
-	<title>Kounting | Home</title>
+	<title>kount.ing | Home</title>
 	<meta name="description" content="Kount, kount, and kount..." />
 </svelte:head>
 
-<section></section>
+<section class="w-full">
+	<h1 class="text-4xl md:text-5xl font-semibold mb-8">kount.ing</h1>
+	<p class="text-xl md:text-2xl tracking-wider text-gray-700 font-light mb-8">
+		what do you want to <span class="tracking-normal font-semibold text-primary">kount</span>?
+	</p>
+	<form method="post" class="w-full flex flex-col items-center justify-center gap-8">
+		<div class="flex w-full md:w-4/5 lg:w-3/5 border-2 h-12 mx-auto rounded-md text-sm md:text-lg">
+			<select value="number of times" class="px-2 bg-primary text-white rounded-sm">
+				<option>number of times</option>
+			</select>
+			<input placeholder="I relapsed" type="text" class="px-4 bg-transparent w-4/5" />
+		</div>
+		<button type="submit" class="bg-primary font-medium text-white rounded-md text-xl px-8 py-2"
+			>kount now!</button
+		>
+	</form>
+</section>
 
 <style>
 	section {
@@ -14,26 +30,6 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		flex: 0.8;
 	}
 </style>
